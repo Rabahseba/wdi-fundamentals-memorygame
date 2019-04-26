b2005eed35c44f88f401c84a40bb98ae55b39bb1
Rabah <r.seba@icloud.com>
2019-04-26 13:32:22 -0400

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,65 @@
 console.log("Up and running!")
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
-	
+
+
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  alert("You found a match!");
+	} else {
+	  alert("Sorry, try again.");
+	}
+};
+
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id')
+	var flippedCard = cards[cardId];
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped" + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	this.setAttribute('src', flippedCard.cardImage);
+
+	if (cardsInPlay.length === 2) {
+		console.log(cardsInPlay)
+		checkForMatch()
+	}
+
+};
+
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard)
+		var gameBoard = document.getElementById('game-board')
+		gameBoard.appendChild(cardElement)
+	}
+}
+
+createBoard()
